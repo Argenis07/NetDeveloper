79c3368e74ea3163fa6866e390ae0bac516806dd
Argenis Salazar <argenis2207@outlook.com>
2023-09-06 22:27:15 -0500

    Se crearon los repositorios

---
+++ DataAccess/Repositories/ArtistRepository.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class ArtistRepository : Repository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(ChinookContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Artist> GetArtistsByStore()
+        {
+            return chinookcontext.Database.SqlQuery<Artist>("GetListaArtista");
+        }
+
+        public Artist GetByName(string name)
+        {
+            return chinookcontext.Artists.FirstOrDefault(artista => artista.Name == name);
+        }
+
+        public ChinookContext chinookcontext
+        {
+            get { return Context as ChinookContext; }
+        }
+    }
+}
---
+++ DataAccess/Repositories/TrackRepository .cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class TrackRepository : Repository<Track>, ITrackRepository
+    {
+        public TrackRepository(DbContext context) : base(context)
+        {
+        }
+
+        public Track GetByName(string name)
+        {
+            return chinookcontext.Track.FirstOrDefault(track => track.Name == name);
+        }
+
+        public ChinookContext chinookcontext
+        {
+            get { return Context as ChinookContext; }
+        }
+    }
+}
